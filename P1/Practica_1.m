fbe60ba5654725d6222126b16d0c870d53e5cb63
César Munuera Pérez <49239950+cesarmunuera@users.noreply.github.com>
2023-02-23 14:45:15 +0100

    ej2 a medias

--- P1/Practica_1.m
+++ P1/Practica_1.m
@@ -1,13 +1,15 @@
 %% INICIALIZACIÓN DE ROS
 % Se definen las variables de entorno ROS_MASTER_URI (ip del Master) y ROS_IP (IP de la máquina donde se ejecuta Matlab). Si se está conectado a la misma red, la variable ROS_IP no es necesario definirla.
-setenv('ROS_MASTER_URI','http://172.22.71.5:11311')
-setenv('ROS_IP','172.22.33.17')
+setenv('ROS_MASTER_URI','http://192.168.1.4:11311')
+setenv('ROS_IP','192.168.1.5')
 rosshutdown
 rosinit % Inicialización de ROS
+
 %% DECLARACIÓN DE SUBSCRIBERS
 odom_sub=rossubscriber('/robot0/odom'); % Subscripción a la odometría
+
 %% Nos aseguramos recibir un mensaje relacionado con el robot "robot0"
-odom = receive(odom_sub, 10)
+odom = receive(odom_sub, 10);
 %%odom
 %%showdetails(odom)
 
@@ -16,19 +18,17 @@ pos=odom.Pose.Pose.Position;
 
 %% DECLARACIÓN DE PUBLISHERS
 pub = rospublisher('/robot0/cmd_vel', 'geometry_msgs/Twist');
+
 %% GENERACIÓN DE MENSAJE
 msg=rosmessage(pub) %% Creamos un mensaje del tipo declarado en "pub"(geometry_msgs/Twist)
-% Rellenamos los campos del mensaje para que el robot avance a 0.2 m/s
 % Velocidades lineales en x,y y z (velocidades en y o z no se usan enrobots diferenciales y entornos 2D)
-msg.Linear.X=0.1;
+msg.Linear.X=0;
 msg.Linear.Y=0;
 msg.Linear.Z=0;
 % Velocidades angulares (en robots diferenciales y entornos 2D solo seutilizará el valor Z)
 msg.Angular.X=0;
-msg.Angular.Y=0;
+msg.Angular.Y=0.3;
 msg.Angular.Z=0;
-
-
 
 %% Definimos la perodicidad del bucle (10 hz)
 r = robotics.Rate(10);
@@ -41,25 +41,28 @@ array_pos = [];
 %% Bucle de control infinito
 while (metros_recorridos < metros_a_recorrer)
 send(pub,msg);
+odom = receive(odom_sub, 10);
 
-odom = receive(odom_sub, 10)
-pos_X=odom.Pose.Pose.Position.X
+pos_X = odom.Pose.Pose.Position.X;
 
 array_pos(contador) = pos_X;
 metros_recorridos = pos_X;
 contador= contador + 1;
+
 % Temporización del bucle según el parámetro establecido en r
 waitfor(r);
 end
 
-%%Calculo de q
+contador
+
+%%Calculo de q (resolucion)
 q_max = 0;
-length(array_pos);
-for i=2:length(array_pos)
-    q= array_pos(i) - array_pos(i-1)
+for i=2:contador-1 
+    q = array_pos(i) - array_pos(i-1);
     if q > q_max    
         q_max = q;
     end
 end
+
 q_max
 
