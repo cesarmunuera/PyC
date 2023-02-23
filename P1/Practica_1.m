c0c7b1a83637874d8eb56df6b4b938bb6b2937e4
victorfernandezt <vic88basket@hotmail.com>
2023-02-23 12:35:05 +0100

    puta

--- P1/Practica_1.m
+++ P1/Practica_1.m
@@ -8,7 +8,8 @@ rosinit % Inicialización de ROS
 odom_sub=rossubscriber('/robot0/odom'); % Subscripción a la odometría
 %% Nos aseguramos recibir un mensaje relacionado con el robot "robot0"
 odom = receive(odom_sub, 10)
-odom
+%%odom
+%%showdetails(odom)
 
 %% Obtenemos la posición actual
 pos=odom.Pose.Pose.Position;
@@ -19,18 +20,46 @@ pub = rospublisher('/robot0/cmd_vel', 'geometry_msgs/Twist');
 msg=rosmessage(pub) %% Creamos un mensaje del tipo declarado en "pub"(geometry_msgs/Twist)
 % Rellenamos los campos del mensaje para que el robot avance a 0.2 m/s
 % Velocidades lineales en x,y y z (velocidades en y o z no se usan enrobots diferenciales y entornos 2D)
-msg.Linear.X=0.2;
+msg.Linear.X=0.1;
 msg.Linear.Y=0;
 msg.Linear.Z=0;
 % Velocidades angulares (en robots diferenciales y entornos 2D solo seutilizará el valor Z)
 msg.Angular.X=0;
 msg.Angular.Y=0;
 msg.Angular.Z=0;
+
+
+
 %% Definimos la perodicidad del bucle (10 hz)
 r = robotics.Rate(10);
+%%Inicializacion del contador y la poscion
+contador = 1;
+metros_a_recorrer = 3;
+metros_recorridos = 0;
+array_pos = [];
+
 %% Bucle de control infinito
-while (1)
+while (metros_recorridos < metros_a_recorrer)
 send(pub,msg);
+
+odom = receive(odom_sub, 10)
+pos_X=odom.Pose.Pose.Position.X
+
+array_pos(contador) = pos_X;
+metros_recorridos = pos_X;
+contador= contador + 1;
 % Temporización del bucle según el parámetro establecido en r
-waitfor(r)
+waitfor(r);
 end
+
+%%Calculo de q
+q_max = 0;
+length(array_pos);
+for i=2:length(array_pos)
+    q= array_pos(i) - array_pos(i-1)
+    if q > q_max    
+        q_max = q;
+    end
+end
+q_max
+
