1698d3dae2bad1a1969027e154ffc8c13e843c31
César Munuera Pérez <49239950+cesarmunuera@users.noreply.github.com>
2023-03-19 18:58:02 +0100

    5.2 terminada

--- P1/laser.m
+++ P1/laser.m
@@ -12,7 +12,7 @@ laser_sub=rossubscriber('/robot0/laser_0'); % Subscripción a la odometría
 pub = rospublisher('/robot0/cmd_vel', 'geometry_msgs/Twist');
 
 %% GENERACIÓN DE MENSAJE
-msg=rosmessage(pub) %% Creamos un mensaje del tipo declarado en "pub"(geometry_msgs/Twist)
+msg=rosmessage(pub); %% Creamos un mensaje del tipo declarado en "pub"(geometry_msgs/Twist)
 % Velocidades lineales en x,y y z (velocidades en y o z no se usan enrobots diferenciales y entornos 2D)
 msg.Linear.X=0;
 msg.Linear.Y=0;
@@ -31,11 +31,10 @@ r = robotics.Rate(10);
 array_izq = [];
 array_cent = [];
 array_der = [];
-mix = [];
 cti = 0;
 ctd = 0;
 ctc = 0;
-dist = 4;
+dist = 3.9;
 sum = 0;
 mi = 0;
 md = 0;
@@ -55,12 +54,12 @@ for i=1:100
     % Primero valoramos la fiabilidad de los sensores ---------------------
     % Dividimos el array de haces entre 3
     laser0 = receive(laser_sub, 10);
-    num_haces = size(laser0.Ranges, 1);
+    num_haces = length(laser0.Ranges);
     tam_array = int32(num_haces / 3);
 
-    array_izq = reshape(laser0.Ranges(1:tam_array), 1, tam_array);
+    array_der = reshape(laser0.Ranges(1:tam_array), 1, tam_array);
     array_cent = reshape(laser0.Ranges(tam_array+1:(tam_array*2)+1), 1, tam_array + 1);
-    array_der = reshape(laser0.Ranges((tam_array*2)+2:num_haces), 1, tam_array);
+    array_izq = reshape(laser0.Ranges((tam_array*2)+2:num_haces), 1, tam_array);
     %Ahora tenemos 3 zonas de trabajo, como 3 laser independientes
 
     for j = 1:length(array_izq) %Recorremos array izq y der -------------------
@@ -99,17 +98,34 @@ for i=1:100
             ctc = ctc + sum;
         end
     end
-
-
-    %Ahora hacemos la media de las probabilidades --------------------------
-
-
 end
 
-mi = cti / length(array_izq);
-md = ctd / length(array_der);
-mc = ctc / length(array_cent);
 
-disp("La fiabilidad del laser por la izquierda es del " + mi + " %.");
-disp("La fiabilidad del laser por la derecha es del " + md + " %.");
-disp("La fiabilidad del laser por el centro es del " + mc + " %.")
+%% Ahora hacemos la media de las probabilidades --------------------------
+mi = cti / 100;
+md = ctd / 100;
+mc = ctc / 100;
+
+disp("La probabilidad de pared por la izquierda es del " + mi + " %.");
+disp("La probabilidad de pared por la derecha es del " + md + " %.");
+disp("La probabilidad de pared por el centro es del " + mc + " %.");
+
+
+%Ahora procedemos a realizar la codificación -----------------------------
+if(mi < 50 && mc > 50 && md < 50)
+    disp("Codificación 1")
+elseif(mi < 50 && mc < 50 && md > 50)
+    disp("Codificación 2")
+elseif(mi > 50 && mc < 50 && md < 50)
+    disp("Codificación 4")
+elseif(mi < 50 && mc > 50 && md > 50)
+    disp("Codificación 5")
+elseif(mi > 50 && mc > 50 && md < 50)
+    disp("Codificación 7")
+elseif(mi > 50 && mc < 50 && md > 50)
+    disp("Codificación 9")
+elseif(mi > 50 && mc > 50 && md > 50)
+    disp("Codificación 14")
+elseif(mi < 50 && mc < 50 && md < 50)
+    disp("Codificación 0")
+end
